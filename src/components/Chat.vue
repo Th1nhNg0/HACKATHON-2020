c255f8fcf7d38bdd7b8e1494a0b27bb3378bb2c4
shoxie <sktt1lka@gmail.com>
2020-12-26 14:06:15 +0700

    fix chat

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -6,36 +6,51 @@
       v-chat-scroll="{ always: false, smooth: true }"
     >
       <li class="message" v-for="n in messages" :key="n">
-        <strong>{{ n.author }}</strong
+        <strong>{{ n.user }}</strong
         >: {{ n.message }}
       </li>
     </ul>
     <div class="flex flex-row w-full pt-5">
-      <input type="text" class="rounded-full w-full" v-model="message"/>
+      <input type="text" class="rounded-full w-full" v-model="message" />
       <button class="bg-green-400 rounded-full px-2" @click="send">Send</button>
     </div>
   </div>
 </template>
 
 <script>
+import { messagesCollection } from "../firebase";
 export default {
   name: "Chat",
   data() {
     return {
-      message: '',
+      message: "",
       messages: [
         {
           message: "asdasd",
-          author: "WR",
+          user: "WR",
         },
       ],
     };
   },
+  mounted() {
+    messagesCollection.onSnapshot((query) => {
+      this.messages = [];
+      query.forEach((q) => {
+        console.log(q.data());
+        this.messages.push(q.data());
+      });
+    });
+  },
   methods: {
     send() {
-
-    }
-  }
+      messagesCollection.add({
+        message: this.message,
+        user: this.$store.state.username,
+        sendAt: Date.now(),
+      });
+      this.message = "";
+    },
+  },
 };
 </script>
 <style scoped>
