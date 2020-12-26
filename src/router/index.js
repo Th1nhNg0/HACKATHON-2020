81b4549aa6f4790f736c4396467cb2b8d12d94b1
th1nhng0 <ngophuthinh2001@gmail.com>
2020-12-26 10:53:23 +0700

    ok

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import { auth } from "../firebase";
 
 Vue.use(VueRouter);
 
@@ -22,18 +23,18 @@ const routes = [
   {
     path: "/login",
     name: "Login",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+    meta: {
+      requiresAuth: false,
+    },
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/Login.vue"),
   },
   {
     path: "/dashboard",
     name: "Dashboard",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+    meta: {
+      requiresAuth: true,
+    },
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/Dashboard.vue"),
   },
@@ -41,6 +42,15 @@ const routes = [
 
 const router = new VueRouter({
   routes,
+  mode: "history",
 });
 
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some((x) => x.meta.requiresAuth);
+  if (requiresAuth && !auth.currentUser) {
+    next("login");
+  } else {
+    next();
+  }
+});
 export default router;
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,62 @@
 <template>
-  <div class="w-screen h-screen flex justify-center">
-    <div>
-      <h1 class="text-3xl">Đăng nhập</h1>
+  <div class="w-screen h-screen flex justify-center items-center bg-custom-bg">
+    <div v-show="state" class="p-10 bg-gray-200 rounded-xl">
+      <h1 class="text-4xl font-bold">Đăng nhập</h1>
+      <form @submit="login" class="text-left flex flex-col gap-1">
+        <p>Email:</p>
+        <input class="rounded-xl px-5 py-2" type="email" v-model="email" />
+        <p>Mật khẩu:</p>
+        <input
+          class="rounded-xl px-5 py-2"
+          type="password"
+          v-model="password"
+        />
+        <button
+          type="submit"
+          class="w-full bg-green-500 py-2 mt-2 rounded-xl text-white font-bold text-xl"
+        >
+          Đăng nhập
+        </button>
+        <p class="mt-5">Chưa có tài khoản?</p>
+        <button
+          @click="state = false"
+          class="w-full bg-blue-500 py-2  rounded-xl text-white font-bold text-xl"
+        >
+          Đăng ký
+        </button>
+      </form>
+    </div>
+    <div v-show="!state" class="p-10 bg-gray-200 rounded-xl">
+      <h1 class="text-4xl font-bold">Đăng ký</h1>
+      <form class="text-left flex flex-col gap-1">
+        <p>Email:</p>
+        <input class="rounded-xl px-5 py-2" type="email" v-model="remail" />
+        <p>Mật khẩu:</p>
+        <input
+          class="rounded-xl px-5 py-2"
+          type="password"
+          v-model="rpassword"
+        />
+        <p>Nhập lại mật khẩu:</p>
+        <input
+          class="rounded-xl px-5 py-2"
+          type="password"
+          v-model="rrepassword"
+        />
+        <button
+          type="submit"
+          class="w-full bg-green-500 py-2 mt-2 rounded-xl text-white font-bold text-xl"
+        >
+          Đăng ký
+        </button>
+        <p class="mt-5">Đã có tài khoản?</p>
+        <button
+          @click="state = true"
+          class="w-full bg-blue-500 py-2  rounded-xl text-white font-bold text-xl"
+        >
+          Đăng Nhấp
+        </button>
+      </form>
     </div>
   </div>
 </template>
@@ -12,6 +67,18 @@
 
 export default {
   name: "Login",
-  components: {},
+  data() {
+    return {
+      state: true,
+      email: "",
+      password: "",
+      rpassword: "",
+      remail: "",
+      rrepassword: "",
+    };
+  },
+  methods: {
+    login() {},
+  },
 };
 </script>
