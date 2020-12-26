6db7a4f38e83528137eb38548db7e7a724a50702
th1nhng0 <ngophuthinh2001@gmail.com>
2020-12-26 10:19:24 +0700

    ok

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div class="flex flex-row justify-between">
+      <div>
+        <span>Gallifrey Training</span>
+      </div>
+      <div class="flex flex-row gap-10">
+        <button>Forum</button>
+        <span>Username</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Dshboard",
+};
+</script>
---
+++ src/views/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="w-screen h-screen flex justify-center">
+    <div>
+      <h1 class="text-3xl">Đăng nhập</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Login",
+  components: {},
+};
+</script>
