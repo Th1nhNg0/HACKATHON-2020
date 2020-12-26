7dcfac75101ad204f48dccedbc6edcee6ef53a41
shoxie <sktt1lka@gmail.com>
2020-12-26 10:47:38 +0700

    dashboard 10%

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,11 +2,23 @@
   <div class="container mx-auto pt-10 bg-custom-bg">
     <div class="flex flex-row justify-between">
       <div>
-        <span class="text-3xl">Gallifrey Training</span>
+        <span class="text-3xl text-white">Gallifrey Training</span>
       </div>
       <div class="flex flex-row gap-10 text-2xl items-center text-white">
-        <button class="bg-green-400 font-bold py-2 px-8 rounded-full">Forum</button>
+        <button class="bg-green-400 font-bold py-2 px-8 rounded-full">
+          Forum
+        </button>
         <span>Username</span>
+      </div>
+    </div>
+    <div>
+      <span class="font-bold text-3xl text-white">Luyện tập:</span>
+    </div>
+    <div class="flex flex-row gap-10 pt-10">
+      <div
+        class="h-40 w-40 bg-gray-500 flex items-center justify-center rounded-lg"
+      >
+        <i class="fad fa-plus text-6xl"></i>
       </div>
     </div>
   </div>
